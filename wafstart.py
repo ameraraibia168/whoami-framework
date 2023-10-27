9b1a6b8acc97d8c9b776a6ffcb33f964eb098f4e
Amerlaceset <46226856+Amerlaceset@users.noreply.github.com>
2023-10-27 19:38:50 +0100

    Update wafstart.py

--- wafstart.py
+++ wafstart.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import os,sys,time
 from waf.libs.banner import *
 from waf.libs.color import *
 from waf.libs.tools import *
 from waf.interpreter import *
-#starting()
+starting()
 banner()
 WhoAmi()
-print (default+"  ERROR")
+

